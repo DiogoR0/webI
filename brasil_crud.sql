66e3ba068ef6b110f43d34729b0142f466d7c887
Diogo Eduardo <110206197+DiogoR0@users.noreply.github.com>
2023-11-09 21:01:14 -0300

    Update brasil_crud.sql
    Alterada algumas descrições

--- brasil_crud.sql
+++ brasil_crud.sql
@@ -1,17 +1,15 @@
--- Criação da base de dados
+
 CREATE DATABASE IF NOT EXISTS `brasil_crud`;
 
--- Seleção da base de dados
+
 USE `brasil_crud`;
 
--- Criação da tabela de Estados
 CREATE TABLE IF NOT EXISTS `estados` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `sigla` VARCHAR(2) NOT NULL,
     `nome` VARCHAR(50) NOT NULL
 );
 
--- Criação da tabela de Cidades
 CREATE TABLE IF NOT EXISTS `cidades` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `nome` VARCHAR(50) NOT NULL,
